6754d14066113040f01c5d0fb697ed543bd5350c
oscar90y5 <oscarfernandezangulo@gmail.com>
2017-05-11 19:55:53 +0200

    No aprende pero casi

--- perceptronMulticapa.cpp
+++ perceptronMulticapa.cpp
@@ -15,13 +15,14 @@ using namespace std;
 //Matrices que almacenan los pesos
 float peso1[NEU_ENTRADA][NEU_OCULTAS]; //pesos entre neuronas de entrada y ocultas.
 float peso2[NEU_OCULTAS][NEU_SALIDA]; //pesos entre neuronas ocultas y de salida.
-float bias = 1.0f; //El bias lo ajustamos al valor 1
+float biasOculto[NEU_OCULTAS];
+float biasSalida[NEU_SALIDA];
 float oculto[NEU_OCULTAS];
 float salida[NEU_SALIDA];
 
-void imprimeSalida(){
+void imprimeSalida(float* sal){
 	for(int i = 0; i<NEU_SALIDA; i++){
-		cout << salida[i] << endl;
+		cout << sal[i] << endl;
 	}
 }
 
@@ -42,15 +43,21 @@ void initPesos(){
 	}
 }
 
+void initBias(){
+	int i;
+	for(i = 0; i<NEU_OCULTAS; i++){
+		biasOculto[i] = 1.0;
+	}
+	for(i = 0; i<NEU_SALIDA; i++){
+		biasSalida[i] = 1.0;
+	}
+}
+
 float sigmoide( float s ){
 	return (1/(1+exp(-s)));
 }
 
-void entrenaRed( float entrada[NEU_ENTRADA], float salida ){
-
-}
-
-float* run( float entrada[NEU_ENTRADA] ){
+float* run( float* entrada ){
 	int i, j;
 
 	for(i=0;i<NEU_OCULTAS;i++){
@@ -66,7 +73,7 @@ float* run( float entrada[NEU_ENTRADA] ){
 		}
 	}
 	for(j = 0; j<NEU_OCULTAS; j++){
-		oculto[j]-=bias;
+		oculto[j]-=biasOculto[i];
 		oculto[j] = sigmoide(oculto[j]);
 	}
 	
@@ -76,17 +83,95 @@ float* run( float entrada[NEU_ENTRADA] ){
 		}
 	}
 	for(j = 0; j<NEU_SALIDA; j++){
-		salida[j]-=bias;
+		salida[j]-=biasSalida[j];
 		salida[j] = sigmoide(salida[j]);
 	}
 	return salida;
 }
 
-int main(){
-	float entrada[2] = {0,1};
-	initPesos();
-	float* salida;
-	salida = run(entrada);
-	imprimeSalida();
+void entrenaRed( float* entrada, float* salida ){
+	int i, j;
+	float* salidaObtenida;
+	float errorSalida[NEU_SALIDA];
+	float errorOculto[NEU_OCULTAS];
+
+	salidaObtenida = run(entrada);
+
+	//Calculamos los errores en la salida y modificamos el bias de cada 
+	// neurona de salida.
+	for(i = 0; i<NEU_SALIDA; i++){
+		errorSalida[i] = salida[i] - salidaObtenida[i];
+		biasSalida[i]-= K*errorSalida[i];
+	}
+
+	//Modificamos los pesos de las conexiones entre las neuronas ocultas y
+	// las salidas en funcion del error.
+	for(i = 0; i<NEU_OCULTAS; i++){
+		for(j = 0; j<NEU_SALIDA; j++){
+			peso2[i][j] += K*errorSalida[j]*oculto[i];
+		}
+	}
+
+	//Inicializamos el error a cero.
+	for(i=0; i<NEU_OCULTAS; i++){
+		errorOculto[i]=0;
+	}
+
+	//Propagamos el error.
+	for(i = 0; i<NEU_OCULTAS; i++){
+		for(j = 0; j<NEU_SALIDA; j++){
+			errorOculto[i]+=errorSalida[j] * peso2[i][j];
+		}
+	}
+	
+	//Modificamos el bias de las neuronas ocultas.
+	for(i = 0; i<NEU_OCULTAS; i++){
+		biasOculto[i]-= K*errorOculto[i];
+	}
+	
+	//Modificamos los pesos de las conexiones entre las neuronas de entrada
+	// y las ocultas.
+	for(i = 0; i<NEU_ENTRADA; i++){
+		for(j = 0; j<NEU_OCULTAS; j++){
+			peso1[i][j] += K*errorOculto[j]*entrada[i];
+		}
+	}
 }
+
+
+int main(){
+	float a[2] = {0,0};
+	float b[2] = {0,1};
+	float c[2] = {1,0};
+	float d[2] = {1,1};
+	float sa[2] = {0};
+	float sb[2] = {1};
+	float sc[2] = {1};
+	float sd[2] = {1};
+	initPesos();
+	initBias();
+	for(int i = 0; i<2000; i++){
+		entrenaRed(a,sa);
+		entrenaRed(b,sb);
+		entrenaRed(c,sc);
+		entrenaRed(d,sd);
+	}
+	float* sal;	
+	sal = run(a);
+	cout << "0,0" << endl;
+	imprimeSalida(sal);
+	
+	sal = run(b);
+	cout << "0,1" << endl;
+	imprimeSalida(sal);
+	
+	sal = run(c);
+	cout << "1,0" << endl;
+	imprimeSalida(sal);
+	
+	sal = run(d);
+	cout << "1,1" << endl;
+	imprimeSalida(sal);
+}
+
 
