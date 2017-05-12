1713c3ee2425d150126b09888d2fb7034efaff12
oscar90y5 <oscarfernandezangulo@gmail.com>
2017-05-12 19:20:37 +0200

    No aprende tan bien... Cambiada la funcion de activacion de las neuronas ocultas por la tangente hiperbolica

--- perceptronMulticapa.cpp
+++ perceptronMulticapa.cpp
@@ -1,4 +1,6 @@
 //Perceptron multicapa (sin retroalimentacion).
+//La funcion de activacion para las neuronas ocultas sera la tangente
+// hiperbolica y para las de salida la sigmoidal.
 #include <cstdlib>
 #include <ctime>
 #include <cmath>
@@ -7,9 +9,9 @@
 using namespace std;
 
 #define NEU_ENTRADA 2  //Numero de neuronas de entrada.
-#define NEU_OCULTAS 4  //Numero de neuronas ocultas.
+#define NEU_OCULTAS 2  //Numero de neuronas ocultas.
 #define NEU_SALIDA 1   //Numero de neuronas de salida.
-#define EPOCA 500      //Numero de iteraciones del entrenamiento.
+#define EPOCA 5000      //Numero de iteraciones del entrenamiento.
 #define K 0.2f         //Taza de aprendizaje del perceptron.
 
 //Matrices que almacenan los pesos
@@ -56,6 +58,10 @@ float sigmoide( float s ){
 	return (1/(1+exp(-s)));
 }
 
+float tanHip( float s){
+	return ((1-exp(-s))/(1+exp(-s)));
+}
+
 float* run( float* entrada ){
 	float* salida = new float[NEU_SALIDA];
 	int i, j;
@@ -74,7 +80,7 @@ float* run( float* entrada ){
 	}
 	for(j = 0; j<NEU_OCULTAS; j++){
 		oculto[j]-=biasOculto[i];
-		oculto[j] = sigmoide(oculto[j]);
+		oculto[j] = tanHip(oculto[j]);
 	}
 	
 	for(i = 0; i<NEU_OCULTAS; i++){
@@ -89,6 +95,44 @@ float* run( float* entrada ){
 	return salida;
 }
 
+float* ej( float* entrada ){
+	float* salida = new float[NEU_SALIDA];
+	int i, j;
+	peso1[0][0]=1.0f;
+	peso1[0][1]=1.0f;
+	peso1[1][0]=1.0f;
+	peso1[1][1]=1.0f;
+	peso2[0][0]=1.0f;
+	peso2[1][0]=-1.5f;
+	biasOculto[0]=-0.5f;
+	biasOculto[1]=-1.5f;
+	biasSalida[0]=-0.5f;
+	for(i=0;i<NEU_OCULTAS;i++){
+		oculto[i]=0.0;
+	}
+	for(i=0;i<NEU_SALIDA;i++){
+		salida[i]=0.0;
+	}
+
+	for(i = 0; i<NEU_ENTRADA; i++){
+		for(j = 0; j<NEU_OCULTAS; j++){
+			oculto[j]+=entrada[i]*peso1[i][j];
+		}
+	}
+	for(j = 0; j<NEU_OCULTAS; j++){
+		oculto[j]-=biasOculto[i];
+	}
+	
+	for(i = 0; i<NEU_OCULTAS; i++){
+		for(j = 0; j<NEU_SALIDA; j++){
+			salida[j]+=oculto[i]*peso2[i][j];
+		}
+	}
+	for(j = 0; j<NEU_SALIDA; j++){
+		salida[j]-=biasSalida[j];
+	}
+	return salida;
+}
 void entrenaRed( float* entrada, float* salida ){
 	int i, j;
 	float* salidaObtenida;
@@ -144,13 +188,13 @@ int main(){
 	float b[2] = {0,1};
 	float c[2] = {1,0};
 	float d[2] = {1,1};
-	float sa[2] = {0};
-	float sb[2] = {1};
-	float sc[2] = {1};
+	float sa[2] = {1};
+	float sb[2] = {0};
+	float sc[2] = {0};
 	float sd[2] = {1};
 	initPesos();
 	initBias();
-	for(int i = 0; i<2000; i++){
+	for(int i = 0; i<EPOCA; i++){
 		entrenaRed(a,sa);
 		entrenaRed(b,sb);
 		entrenaRed(c,sc);
@@ -158,18 +202,22 @@ int main(){
 	}
 	float* salida;	
 	salida = run(a);
+	//salida = ej(a);
 	cout << "0,0" << endl;
 	imprimeSalida(salida);
 	
 	salida = run(b);
+	//salida = ej(b);
 	cout << "0,1" << endl;
 	imprimeSalida(salida);
 	
 	salida = run(c);
+	//salida = ej(c);
 	cout << "1,0" << endl;
 	imprimeSalida(salida);
 	
 	salida = run(d);
+	//salida = ej(d);
 	cout << "1,1" << endl;
 	imprimeSalida(salida);
 }
